fb4a28b2aadfa928007567550000adf57c54ca1a
cribr-1 <cribr.admin@gmail.com>
2026-08-13 16:21:17 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import { initGA4 } from './lib/gtag.ts';
@@ -8,6 +9,10 @@ initGA4();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
+
+
